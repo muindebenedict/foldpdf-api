6798872440f95881b8f4c25d7cc2816b1197e1fa
muindebenedict <muindebenedictblogs@gmail.com>
2026-07-21 08:38:52 +0300

    Fix logging level for webhook debugging

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from datetime import datetime
 from flask import Flask, request, send_file, jsonify
 from flask_cors import CORS
 
+logging.basicConfig(level=logging.INFO)
+
 app = Flask(__name__)
 CORS(app)
 
